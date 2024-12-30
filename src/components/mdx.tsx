e61bece15aa52eb7e1c09fc13ad577a433725a22
Dylan Barduzzi <dlbarduzzi@gmail.com>
2024-12-30 13:35:21 -0600

    Add anchor link

--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -18,10 +18,41 @@ const h1 = ({ ...props }: HTMLProps<HTMLHeadingElement>) => {
 }
 
 const h2 = ({ ...props }: HTMLProps<HTMLHeadingElement>) => {
+  const isAnchorLink = props.id && props.id.trim().length > 0
+  if (isAnchorLink) {
+    return (
+      <h2
+        className="group relative mb-6 mt-10 flex items-center bg-gray-100 font-head text-2xl tracking-tight first:mt-0 md:text-2xl"
+        {...props}
+      >
+        <span className="absolute -left-6 hidden text-rose-500 group-hover:inline-block">
+          {" "}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="size-5"
+          >
+            <line x1="4" x2="20" y1="9" y2="9" />
+            <line x1="4" x2="20" y1="15" y2="15" />
+            <line x1="10" x2="8" y1="3" y2="21" />
+            <line x1="16" x2="14" y1="3" y2="21" />
+          </svg>
+        </span>
+        <a href={`#${props.id}`} className="font-bold text-rose-500 no-underline">
+          {props.children}
+        </a>
+      </h2>
+    )
+  }
   return (
     <h2
       className={cn(
-        "mb-6 mt-10 font-head text-2xl font-bold tracking-tight text-black",
+        "mb-6 mt-10 bg-gray-100 font-head text-2xl font-bold tracking-tight",
         "text-rose-500 first:mt-0 md:text-2xl"
       )}
       {...props}
